87ebcb9162cb9c81d33f0e311b7a246387bfbd47
TakaoWing <tt.wing2838783@gmail.com>
2020-05-18 16:05:56 +0900

    first commit

---
+++ Assets/Scripts/ExprosionGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExprosionGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private string hitTag;
+    [SerializeField]
+    private GameObject exprosion;
+    private GameObject exp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == hitTag) {
+            exp = Instantiate(exprosion,this.transform.position,Quaternion.identity);
+            Destroy(exp, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ui;
+    [SerializeField]
+    private PlayerController playerController;
+    [SerializeField]
+    private StoneGenerator stoneGenerator;
+    [SerializeField]
+    private BulletGenerator bulletGenerator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver() {
+        playerController.enabled = false;
+        stoneGenerator.enabled = false;
+        bulletGenerator.enabled = false;
+        ui.SetActive(true);
+    }
+}
